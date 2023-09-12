babb284f8cb295903e4fab128801e77f0474180a
neko967 <113199838+neko967@users.noreply.github.com>
2023-09-12 15:49:23 +0900

    Add: firebaseと連携した雛形を作成

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ isAuth }) => {
   return (
     <nav>
       <Link to="/">ホーム</Link>
-      <Link to="/createpost">記事投稿</Link>
-      <Link to="/login">ログイン</Link>
+      {!isAuth ? (
+        <Link to="/login">ログイン</Link>
+      ) : (
+        <>
+          <Link to="/createpost">記事投稿</Link>
+          <Link to="/logout">ログアウト</Link>
+        </>
+      )}
     </nav>
-  )
+  );
 }
 
 export default Navbar
