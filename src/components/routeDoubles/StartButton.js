52dd09a445fbb6fdfd8826d2dab724975201e64e
neko967 <113199838+neko967@users.noreply.github.com>
2023-09-16 12:14:45 +0900

    Fix: ボタンの大きさを変更

--- src/components/routeDoubles/StartButton.js
+++ src/components/routeDoubles/StartButton.js
@@ -7,7 +7,8 @@ export default function StartButton({ isStart, onStartClick}) {
     <>
     {!isStart && 
       <Button 
-        variant="contained"  
+        variant="contained"
+        size="large"
         sx={{ 
           position: 'fixed', 
           bottom: 60, 
