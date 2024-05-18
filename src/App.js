17e206655aeec5f4e85b4e5cff11b4654150b33c
neko967 <113199838+neko967@users.noreply.github.com>
2024-05-19 08:15:21 +0900

    修正

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Home from './components/routesHome/Home';
 import Singles from './components/routeSingles/SinglesScoreboard';
 import Doubles from './components/routeDoubles/DoublesScoreboard';
 import Record from './components/routeRecord/Records';
-import Admin from './components/routeAdmin/Admin';
 import Member from './components/routeMember/Members';
 import Homey from './components/Homey';
 import CreatePost from './components/CreatePost';
@@ -21,7 +20,6 @@ function App() {
   const [doublesLeftDownPlayer, setDoublesLeftDownPlayer] = useState("名無しB");
   const [doublesRightUpPlayer, setDoublesRightUpPlayer] = useState("名無しC");
   const [doublesRightDownPlayer, setDoublesRightDownPlayer] = useState("名無しD");
-  const [isAdmin, setIsAdmin] = useState(false);
 
   return (
     <>
@@ -35,7 +33,6 @@ function App() {
                                          setDoublesRightUpPlayer={setDoublesRightUpPlayer}
                                          setDoublesRightDownPlayer={setDoublesRightDownPlayer}
                                          isAuth={isAuth}
-                                         isAdmin={isAdmin}
                                    />}
           />
           <Route path="/singles" element={<Singles leftPlayer={singlesLeftPlayer} 
@@ -55,17 +52,13 @@ function App() {
                                           />}
           />
           <Route path="/record" element={<Record />} />
-          <Route path="/admin" element={<Admin setIsAdmin={setIsAdmin}/>} />
           <Route path="/member" element={<Member />} />
-
           <Route path="/homey" element={<Homey/>}></Route>
           <Route path="/createpost" element={<CreatePost isAuth={isAuth}/>}></Route>
         </Routes>
         <BottomNavigation />
       </Router>
     </>
-    
-    
   );
 }
 
